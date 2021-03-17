60485a4fa8232f83252e971bd42aa86b10061f36
Sisirashok <sisira@techversantinfo.com>
2021-03-17 16:22:10 +0530

    commit

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -1,34 +1,34 @@
-var app = window.app = {};
-app.Tasks = function() {
-  this._input = $('#Tasks-search-txt');
-  this._initAutocomplete();
-};
+// var app = window.app = {};
+// app.Tasks = function() {
+//   this._input = $('#Tasks-search-txt');
+//   this._initAutocomplete();
+// };
 
-app.Tasks.prototype = {
+// app.Tasks.prototype = {
 
-};
+// };
 
-_initAutocomplete: function() {
-  this._input
-    .autocomplete({
-      source: '/tasks',
-      appendTo: '#tasks-search-results',
-      select: $.proxy(this._select, this)
-    })
-    .autocomplete('instance')._renderItem = $.proxy(this._render, this);
-}
-_select: function(e, ui) {
-  this._input.val(ui.item.todo + ' - ' + ui.item.category);
-  return false;
-}
-_render: function(ul, item) {
-  var markup = [
+// _initAutocomplete: function() {
+//   this._input
+//     .autocomplete({
+//       source: '/tasks',
+//       appendTo: '#tasks-search-results',
+//       select: $.proxy(this._select, this)
+//     })
+//     .autocomplete('instance')._renderItem = $.proxy(this._render, this);
+// }
+// _select: function(e, ui) {
+//   this._input.val(ui.item.todo + ' - ' + ui.item.category);
+//   return false;
+// }
+// _render: function(ul, item) {
+//   var markup = [
   
-    '<span class="todo">' + item.todo + '</span>',
-    '<span class="category">' + item.category + '</span>',
+//     '<span class="todo">' + item.todo + '</span>',
+//     '<span class="category">' + item.category + '</span>',
  
-  ];
-  return $('<li>')
-    .append(markup.join(''))
-    .appendTo(ul);
-}
+//   ];
+//   return $('<li>')
+//     .append(markup.join(''))
+//     .appendTo(ul);
+// }
